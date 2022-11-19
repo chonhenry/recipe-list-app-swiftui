5ba6c036f5237b4160d8499a1a158a9df991d599
chonhenry <chonhenry@gmail.com>
2022-11-19 16:55:12 -0500

    Initial Commit

---
+++ Recipe List App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipe List App
+//
+//  Created by Henry Chon on 11/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Henry Chon on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
